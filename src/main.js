f35027a7a3250554fb5ff6665d9735ed3317b974
ra178308 <ra178308@ucdb.br>
2023-10-19 20:32:14 -0400

    aula19-10

--- src/main.js
+++ src/main.js
@@ -1,9 +1,47 @@
-import express from 'express';
-import { Category } from './models/category.js'
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const { Category } = require('./models/category.js')
+
+const db = new sqlite3.Database('mocaliza_db.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS categories (id INTERGER PRIMARY KEY, name TEXT)");
+    db.get("SELECT count(id) as total from categories GROUP BY id", null, (err, row) => {
+        if (err) {
+            throw err;
+        }
+        if (row.count == 0){
+            db.run("INSERT INTO categories(name) VALUES('Carros')");
+            db.run("INSERT INTO categories(name) VALUES('Motos')");
+            db.run("INSERT INTO categories(name) VALUES('SUVs')");
+            db.run("INSERT INTO categories(name) VALUES('Hatch')");
+            db.run("INSERT INTO categories(name) VALUES('Sedan')");
+        }
+    });
+});
 
 const app = express();
 
 app.get('/', (request, response) => {
+    db.serialize(() => {
+        const categories = [];
+        db.each("SELECT id, name from categories", (err, row) => {
+            if (err) {
+                throw err;
+            }
+
+            const categoryData = new Category(row.id, row.name);
+
+            categories.push(category);
+
+        });
+
+        response.json({
+            categories,
+        });
+
+    })
+
     response.json({
         data: Date.now(),
     });
