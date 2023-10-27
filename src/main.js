89231f309c494d9e53ac8f6af3d8d4e0ca41bdf1
ra178308 <ra178308@ucdb.br>
2023-10-26 20:29:06 -0400

    aula 26-10

--- src/main.js
+++ src/main.js
@@ -1,54 +1,12 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
-const { Category } = require('./models/category.js')
+const { Database } = require('./configs/sequelize')
 
-const db = new sqlite3.Database('mocaliza_db.db');
-
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS categories (id INTERGER PRIMARY KEY, name TEXT)");
-    db.get("SELECT count(id) as total from categories GROUP BY id", null, (err, row) => {
-        if (err) {
-            throw err;
-        }
-        if (row.count == 0){
-            db.run("INSERT INTO categories(name) VALUES('Carros')");
-            db.run("INSERT INTO categories(name) VALUES('Motos')");
-            db.run("INSERT INTO categories(name) VALUES('SUVs')");
-            db.run("INSERT INTO categories(name) VALUES('Hatch')");
-            db.run("INSERT INTO categories(name) VALUES('Sedan')");
-        }
-    });
-});
+const db = Database.getInstance();
 
 const app = express();
 
-app.get('/', (request, response) => {
-    db.serialize(() => {
-        const categories = [];
-        db.each("SELECT id, name from categories", (err, row) => {
-            if (err) {
-                throw err;
-            }
-
-            const categoryData = new Category(row.id, row.name);
-
-            categories.push(category);
-
-        });
-
-        response.json({
-            categories,
-        });
-
-    })
-
-    response.json({
-        data: Date.now(),
-    });
-});
-
-app.listen(8000, () =>{
-    const c = Category.from(1, 'Cars', true, Date.now(), Date.now());
-    console.log(c);
+app.listen(8000, async() =>{
+    require('./models/category');
+    await db.sync();
     console.log('server is running on port 8000');
 });
