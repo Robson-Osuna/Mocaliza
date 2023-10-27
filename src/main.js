8823a75c693fb713cbdea724a1ffcea51af571f8
ra178308 <ra178308@ucdb.br>
2023-10-26 20:39:51 -0400

    aula 26-10

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const { Database } = require('./configs/sequelize')
-
+const { Category } = require('./models/category');
 const db = Database.getInstance();
 
 const app = express();
 
+app.get('/api/categories', async (req, res) => {
+    const categories = await Category.findAll();
+    res.json({
+        data: categories
+    });
+});
+
+app.post('/api/categories', async (req, res) => {
+    const category = await Category.create(req.params);
+
+    res.json({
+        data: category
+    });
+});
+
 app.listen(8000, async() =>{
-    require('./models/category');
+    
     await db.sync();
     console.log('server is running on port 8000');
 });
